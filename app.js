d4fc42044cae959e94675fe89d77d474f235c5e3
Peter Biskupski <pete.biskupski@gmail.com>
2021-05-13 14:40:27 -0400

    added jQuery as dependency; changed pw in db connection string

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://petebiskupski:eat24now@cluster0.ct74w.mongodb.net/tlc?retryWrites=true&w=majority';
+var mongoDB = 'mongodb+srv://petebiskupski:WbH31IeU4DNpxuzU@cluster0.ct74w.mongodb.net/tlc2?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
